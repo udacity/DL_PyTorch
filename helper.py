dfc978649e13abd7abb9b691cc69167cc52a79bc
Mat Leonard <leonard.mat@gmail.com>
2018-04-09 11:10:41 -0700

    Make normalization optional.

--- helper.py
+++ helper.py
@@ -3,6 +3,7 @@ import numpy as np
 from torch import nn, optim
 from torch.autograd import Variable
 
+
 def test_network(net, trainloader):
 
     criterion = nn.MSELoss()
@@ -27,16 +28,26 @@ def test_network(net, trainloader):
     return True
 
 
-def imshow(image, ax=None, title=None):
-    ''' plt.imshow for Tensor '''
+def imshow(image, ax=None, title=None, normalize=True):
+    """Imshow for Tensor."""
     if ax is None:
         fig, ax = plt.subplots()
     image = image.numpy().transpose((1, 2, 0))
-    mean = np.array([0.5, 0.5, 0.5])
-    std = np.array([0.5, 0.5, 0.5])
-    image = std * image + mean
-    image = np.clip(image, 0, 1)
+    
+    if normalize:
+        mean = np.array([0.485, 0.456, 0.406])
+        std = np.array([0.229, 0.224, 0.225])
+        image = std * image + mean
+        image = np.clip(image, 0, 1)
+    
     ax.imshow(image)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.tick_params(axis='both', length=0)
+    ax.set_xticklabels('')
+    ax.set_yticklabels('')
     
     return ax
 
