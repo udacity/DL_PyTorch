9a31dfd7e863664269a4ff944a87b70d3f0908c1
Mathew Leonard <leonard.mat@gmail.com>
2018-04-26 17:31:52 -0700

    Update for PyTorch v0.4

--- helper.py
+++ helper.py
@@ -33,13 +33,13 @@ def imshow(image, ax=None, title=None, normalize=True):
     if ax is None:
         fig, ax = plt.subplots()
     image = image.numpy().transpose((1, 2, 0))
-    
+
     if normalize:
         mean = np.array([0.485, 0.456, 0.406])
         std = np.array([0.229, 0.224, 0.225])
         image = std * image + mean
         image = np.clip(image, 0, 1)
-    
+
     ax.imshow(image)
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
@@ -48,33 +48,47 @@ def imshow(image, ax=None, title=None, normalize=True):
     ax.tick_params(axis='both', length=0)
     ax.set_xticklabels('')
     ax.set_yticklabels('')
-    
+
     return ax
 
 
 def view_recon(img, recon):
-    ''' Function for displaying an image (as a PyTorch Tensor) and its 
-        reconstruction also a PyTorch Tensor 
+    ''' Function for displaying an image (as a PyTorch Tensor) and its
+        reconstruction also a PyTorch Tensor
     '''
-    
+
     fig, axes = plt.subplots(ncols=2, sharex=True, sharey=True)
     axes[0].imshow(img.numpy().squeeze())
     axes[1].imshow(recon.data.numpy().squeeze())
     for ax in axes:
         ax.axis('off')
         ax.set_adjustable('box-forced')
-        
-def view_classify(img, ps):
+
+def view_classify(img, ps, version="MNIST"):
     ''' Function for viewing an image and it's predicted classes.
     '''
     ps = ps.data.numpy().squeeze()
 
-    fig, (ax1, ax2) = plt.subplots(figsize=(6,7), ncols=2)
-    ax1.imshow(img.numpy().squeeze())
+    fig, (ax1, ax2) = plt.subplots(figsize=(6,9), ncols=2)
+    ax1.imshow(img.resize_(1, 28, 28).numpy().squeeze())
     ax1.axis('off')
     ax2.barh(np.arange(10), ps)
     ax2.set_aspect(0.1)
     ax2.set_yticks(np.arange(10))
-    ax2.set_yticklabels(np.arange(10).astype(int), size='large');
+    if version == "MNIST":
+        ax2.set_yticklabels(np.arange(10))
+    elif version == "Fashion":
+        ax2.set_yticklabels(['T-shirt/top',
+                            'Trouser',
+                            'Pullover',
+                            'Dress',
+                            'Coat',
+                            'Sandal',
+                            'Shirt',
+                            'Sneaker',
+                            'Bag',
+                            'Ankle Boot'], size='small');
     ax2.set_title('Class Probability')
     ax2.set_xlim(0, 1.1)
+
+    plt.tight_layout()
